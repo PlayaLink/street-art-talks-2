61e234ea77545a93e2d13bccca0ae8745791f6b3
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2018-12-28 09:43:02 -0800

    Adds changes to App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import skyline from './img/arts-district-dtla-skyline.jpg';
+import angelinaChristina1 from './img/angelina-christina-1.jpg';
+import angelinaChristina1_audio from './audio/AngelinaChristina1.mp3'
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const MuralList = () => (
+  <div>
+    <Link to="/angelina-christina">
+      <img src={angelinaChristina1} />
+      <span>Two faces</span>
+    </Link>
+  </div>
+)
+
+const Home = () => (
+  <div>
+    <header className="App-header">
+      {/*<img src={skyline} className="App-logo" alt="logo" />*/}
+      <h2 className="page-title">
+        Street Art Talks
+      </h2>
+      <p>
+        An audio tour of DTLA's arts district
+      </p>
+    </header>
+    <MuralList/>
+  </div>
+)
+
+const MuralDetail = (props) => (
+  <div>
+    <img src={props.image}/>
+    <div>{props.title}</div>
+    <div>{props.location}</div>
+    <div>Artist: {props.name}</div>
+    <audio controls>
+      <source src={props.audio} type="audio/mpeg" />
+    </audio>
+    <div>
+      <Link to="/">Go back</Link>
+    </div>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <div>
+            <Route exact path='/' component={Home}/>
+            <Route
+              exact
+              path='/angelina-christina'
+              render={(props) => (
+                <MuralDetail {...props}
+                             name="Angelina Christina"
+                             image={angelinaChristina1}
+                             title="Two faces embracing."
+                             location="4th and Merrick."
+                             audio={angelinaChristina1_audio}
+                />
+              )}
+            />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
